39ad08606ddad8075fe4576f8b395bfe251f248b
Vasudev <tech3in@live.com>
2019-06-11 13:01:51 +0530

    back nav added

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
-import {View, TextInput, Button} from 'react-native';
+import {View, TextInput, Button, Text} from 'react-native';
 
 export default class Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {textInputValue:''};
+    }
 
     static navigationOptions = {
         title:"Home",
@@ -13,10 +18,19 @@ export default class Home extends Component{
         }
     }
 
+    handleBtn = () => {
+        this.props.navigation.navigate('NextView',{text:this.state.textInputValue, rollnum:233, backBtn:this.handleBackCall.bind(this)});
+    }
+
+    handleBackCall = (value) => this.setState({textInputValue:value});
+
+    hanldeTextField = (txt) => this.setState({textInputValue:txt})
+
     render(){
         return <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <TextInput placeholder="Enter something here" />
-            <Button title="Send & Move to Next Screen" />
+            <TextInput placeholder="Enter something here" onChangeText={this.hanldeTextField} />
+            <Button title="Send & Move to Next Screen" onPress={this.handleBtn}/>
+            <Text>{this.state.textInputValue}</Text>
         </View>
     }
 }
--- screens/NextScreen.js
+++ screens/NextScreen.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, Button} from 'react-native';
 
 export default class NextScreen extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {nextInputValue:'Type to send back'};
+    }
+
+    handleTextInputChange = (t) => this.setState({nextInputValue:t});
+
+    handleBackButton = () => {
+        this.props.navigation.state.params.backBtn(this.state.nextInputValue);
+        this.props.navigation.goBack();
+    }
+
     render(){
         return <View style={{flex:1, justifyContent: 'center', alignItems:'center'}}>
-            <Text>Next Screen</Text>
+            <Text style={{fontSize:25}}>{this.props.navigation.getParam('text','a default value')}</Text>
+            <TextInput placeholder="Enter some text" onChangeText={this.handleTextInputChange}/>
+            <Button title="Send Data Back" onPress={this.handleBackButton} />
         </View>
     }
 }
